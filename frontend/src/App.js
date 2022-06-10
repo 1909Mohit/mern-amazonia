a3e28f443a315fe9d9cc3b3c009b6f07b5a33227
Mohit <mohit1909.mk@gmail.com>
2022-06-10 11:45:53 +0530

    data-json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,37 @@
+import data from "./data";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 const App = () => {
   return (
+    <Router>
     <div >
       <header>
         <a href="/">amazona</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products" >
+          {
+            data.products.map((product) => {
+              return (
+                <div className="product" key={product.slug}>
+                  <a href={`/product/${product.slug}`}>
+                    <img src={product.image} alt={product.name} />
+                  </a>
+                  <div className="product-info">
+                    <a href={`/product/${product.slug}`}>
+                      <p>{product.name} </p>
+                    </a>
+                    <p><strong> ${product.price}</strong> </p>
+                    <button>Add to cart</button>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
       </main>
-    </div>
+      </div>
+      </Router>
   );
 }
 
